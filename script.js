69d3fc5c1f592e0caefeac44fa60389aaed0946e
Abhishek <62891992+Abhishek11800356@users.noreply.github.com>
2025-03-08 22:11:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
 // your code here
+	if(num==1)
+		return 0
+	else if(num==2)
+		return 1
+	else
+	{
+		let x=0,y=1,fibNo=0
+		for(let i=3;i<=num;i++)
+			{
+				fibNo= x+y
+				x=y
+				y=fibNo
+			}
+		return fibNo
+	}
 }
 
 module.exports = fibonacci;
